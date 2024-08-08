1578364c40a2ffbc0daae6a44ec8a54625d876f2
Maksim Yavorets <styn1sss@gmail.com>
2024-08-08 12:55:04 +0300

    Fixed adaptive layout

--- script.js
+++ script.js
@@ -1,56 +1,76 @@
-window.onload = function () {
-    var header = document.querySelector("header");
-    var headerImage = document.getElementById("header-bg");
-
-    header.style.height = headerImage.offsetHeight + "px";
-};
-
 var iconHamburger = document.getElementById("icon-hamburger");
 var dropdownMenu = document.querySelector(".dropdown-menu");
 
-document.addEventListener("DOMContentLoaded", function() {
+function updateImages() {
+  var windowWidth = window.innerWidth;
+  var images = document.querySelectorAll(".changeable-image");
+  var mainSection = document.getElementById("main-section");
+  var graphicDesignCard = document.getElementById("card-1");
+  var photographyDesignCard = document.getElementById("card-2");
 
-    iconHamburger.addEventListener("click", function(event) {
-        event.preventDefault();
-        if (dropdownMenu.style.display === "block"){
-            dropdownMenu.style.display = "none";
-        }
-        else{
-            dropdownMenu.style.display = "block"; 
-        }
+  if (windowWidth <= 768) {
+    if (windowWidth <= 490) {
+      changeToMobileImages(mainSection, graphicDesignCard, photographyDesignCard);
+    } else {
+      changeToDesktopImages(mainSection, graphicDesignCard, photographyDesignCard);
+    }
+
+    images.forEach(function (image, index) {
+      var imageName = image.src.split("/").pop();
+      var mobileImagePath = "./images/mobile/" + imageName;
+      image.src = mobileImagePath;
     });
-});
-
-window.addEventListener('resize', function() {
-    var windowWidth = window.innerWidth;
-
-    if (windowWidth <= 768) {
-   
-        var images = document.querySelectorAll('.product-gallery img');
-
-        images.forEach(function(image, index) {
-            
-            var imageName = image.src.split('/').pop(); 
-            var mobileImagePath = './images/mobile/' + imageName;
-            
-            image.src = mobileImagePath;
-        });
+  } else {
+    if (dropdownMenu.style.display === "block") {
+      dropdownMenu.style.display = "none";
     }
-    else{
 
-        if (dropdownMenu.style.display === "block"){
-            dropdownMenu.style.display = "none";
-        }
+    images.forEach(function (image, index) {
+      var imageName = image.src.split("/").pop();
+      var desktopImagePath = "./images/desktop/" + imageName;
+      image.src = desktopImagePath;
+    });
+  }
+}
 
-        var images = document.querySelectorAll('.product-gallery img');
+function toggleMobileMenu(event) {
+  if (dropdownMenu.style.display === "block") {
+    dropdownMenu.style.display = "none";
+  } else {
+    dropdownMenu.style.display = "block";
+  }
+}
 
-        images.forEach(function(image, index) {
+function changeBackgroundImage(element, imageUrl) {
+  element.style.backgroundImage = `url(${imageUrl})`;
+}
 
-            var imageName = image.src.split('/').pop(); 
-            var desktopImagePath = './images/desktop/' + imageName;
-            
-            image.src = desktopImagePath;
-        });
-    }
+function changeToMobileImages(mainSection, graphicDesignCard, photographyDesignCard) {
+  changeBackgroundImage(mainSection, "./images/mobile/image-header.jpg");
+  changeBackgroundImage(
+    graphicDesignCard,
+    "./images/mobile/image-graphic-design.jpg"
+  );
+  changeBackgroundImage(
+    photographyDesignCard,
+    "./images/mobile/image-photography.jpg"
+  );
+}
+
+function changeToDesktopImages(mainSection, graphicDesignCard, photographyDesignCard) {
+  changeBackgroundImage(mainSection, "./images/desktop/image-header.jpg");
+  changeBackgroundImage(
+    graphicDesignCard,
+    "./images/desktop/image-graphic-design.jpg"
+  );
+  changeBackgroundImage(
+    photographyDesignCard,
+    "./images/desktop/image-photography.jpg"
+  );
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  updateImages();
+  window.addEventListener("resize", updateImages);
+  iconHamburger.addEventListener("click", toggleMobileMenu);
 });
-
